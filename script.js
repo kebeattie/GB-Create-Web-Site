9f9e319bc957956e431226e726551b779a7eef40
kebeattie <kebeattie98@hotmail.com>
2024-10-03 10:18:34 +0100

    Added preloader

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var x = document.getElementById("links");
 
 function myFunction() {
-  
+
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
@@ -14,3 +14,9 @@ window.matchMedia("(orientation: landscape)").addEventListener("change", e => {
   x.style.display = "none";
 });
 
+
+// Wait until the entire page (including images) has loaded
+window.onload = function () {
+  var preloader = document.getElementById('preloader');
+  preloader.style.display = 'none'; // Hide preloader
+};
